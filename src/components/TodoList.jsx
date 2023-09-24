c1753a90adf2f19d8a9993c45540e52f18ac9fdc
DavidAdjei <97539347+DavidAdjei@users.noreply.github.com>
2023-09-24 00:43:56 +0000

    added a class

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -39,7 +39,7 @@ function TodoList({ todos, completeTodo, theme, setTodos }) {
           <li onClick={() => setFilter('Active')} className={`link ${filter=== 'Active' ? 'active' : ''}`}>Active</li>
           <li onClick={() => setFilter('Complete')} className={`link ${filter=== 'Complete' ? 'active' : ''}`}>Complete</li>
         </ul>
-        <p onClick={removeCompletedTodos}>Clear Completed</p>
+        <p onClick={removeCompletedTodos} className='remove'>Clear Completed</p>
       </div>
     </div>
   );
